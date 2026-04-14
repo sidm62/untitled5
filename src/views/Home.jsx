a2500c93bd7ee4c86420a97574631b42068f9e18
sidm62 <sidiiqm@metropolia.fi>
2026-04-14 11:16:08 +0300

    Add fetchData utility and update Home component to fetch media data

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from 'react';
+import { fetchData } from '../utils/fetchData';
+import MediaRow from '../components/MediaRow';
+
 const Home = () => {
+
+    const [mediaArray, setMediaArray] = useState([]);
+
+    useEffect(() => {
+        const getMedia = async () => {
+            try {
+
+                const json = await fetchData('test.json');
+                setMediaArray(json);
+                console.log('Media haettu:', json);
+            } catch (error) {
+                console.error('getMedia failed:', error.message);
+            }
+        };
+
+        getMedia();
+    }, []);
+
     return (
-        <div className="space-y-4">
-            <h1 className="text-3xl font-bold">Home View</h1>
-            <p>Tervetuloa sovellukseen!</p>
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Home View</h1>
+            <div className="grid gap-4">
+                {mediaArray.map((item) => (
+
+                    <MediaRow key={item.media_id || item.id} item={item} />
+                ))}
+            </div>
         </div>
     );
 };
