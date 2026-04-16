be9aa0d1c99999434bdf051e2e2fa2e0379ae231
sidm62 <sidiiqm@metropolia.fi>
2026-04-16 12:43:02 +0300

    Implement file upload functionality and update media handling in Upload component

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -1,9 +1,90 @@
+import { useState } from 'react';
+import useForm from '../hooks/formHooks';
+import { useFile, useMedia } from '../hooks/apiHooks';
+import { useNavigate } from 'react-router';
+
 const Upload = () => {
+    const [file, setFile] = useState(null);
+    const { postFile } = useFile();
+    const { postMedia } = useMedia();
+    const navigate = useNavigate();
+
+    const doUpload = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            // 1. Lataa tiedosto tiedostopalvelimelle
+            const fileData = await postFile(file, token);
+            // 2. Tallenna median tiedot Media API:iin
+            // eslint-disable-next-line react-hooks/immutability
+            await postMedia(fileData.data, inputs, token);
+
+            navigate('/');
+        } catch (e) {
+            console.log(e.message);
+            alert('Upload failed: ' + e.message);
+        }
+    };
+
+    const { inputs, handleInputChange, handleSubmit } = useForm(doUpload, {
+        title: '',
+        description: '',
+    });
+
+    const handleFileChange = (evt) => {
+        if (evt.target.files && evt.target.files[0]) {
+            setFile(evt.target.files[0]);
+        }
+    };
+
     return (
-        <div className="space-y-4">
-            <h1 className="text-3xl font-bold">Home View</h1>
-            <p>Tervetuloa sovellukseen!</p>
-        </div>
+        <>
+            <h1>Upload</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input
+                        name="title"
+                        type="text"
+                        id="title"
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        name="description"
+                        rows={5}
+                        id="description"
+                        onChange={handleInputChange}
+                    ></textarea>
+                </div>
+                <div>
+                    <label htmlFor="file">File</label>
+                    <input
+                        name="file"
+                        type="file"
+                        id="file"
+                        accept="image/*, video/*"
+                        onChange={handleFileChange}
+                    />
+                </div>
+                <img
+                    src={
+                        file
+                            ? URL.createObjectURL(file)
+                            : 'https://placehold.co/200?text=Choose+image'
+                    }
+                    alt="preview"
+                    width="200"
+                />
+                <button
+                    type="submit"
+                    disabled={file && inputs.title.length > 3 ? false : true}
+                >
+                    Upload
+                </button>
+            </form>
+        </>
     );
 };
 
